9b465dd85754ce819a8ac915bf98f97ce12e2d55
unknown <sherlinkuo@gmail.com>
2021-12-21 14:26:53 +0800

    第一步

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+
+  // name = "王大明";
+
+  profile = {
+    name : ''
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  check(){
+
+    // 一般使用 html 取值得方式，input 加入 屬性 id="name"
+    /*
+    let temp = <HTMLInputElement> document.getElementById("name");
+    console.log(temp.value);
+    */
+
+    //雙向數據
+    //console.log(this.name);
+
+  }
+
+}
